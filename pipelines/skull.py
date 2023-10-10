895cbcf5916367d8629f013d6dfb0e4db8aecfc7
David Meunier <david_meunier_79@hotmail.fr>
2023-10-10 09:43:59 +0200

    petra_skull_auto_mask

--- pipelines/skull.py
+++ pipelines/skull.py
@@ -736,11 +736,11 @@ def create_skull_petra_pipe(name="skull_petra_pipe", params={}):
 
         if "petra_fast" in params.keys():
             skull_petra_pipe.connect(petra_fast, "restored_image",
-                                  petra_skull_mask_thr, "in_file")
+                                  petra_skull_auto_mask, "in_file")
 
         elif "petra_debias" in params.keys():
             skull_petra_pipe.connect(petra_debias, "output_image",
-                                  petra_skull_mask_thr, "in_file")
+                                  petra_skull_auto_mask, "in_file")
 
 
         skull_petra_pipe.connect(
