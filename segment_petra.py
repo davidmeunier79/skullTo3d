cc76510971b105babd7614307874db1c7cd49f07
David Meunier <david_meunier_79@hotmail.fr>
2023-10-13 14:02:00 +0200

    no stereo_to_native

--- segment_petra.py
+++ segment_petra.py
@@ -612,10 +612,6 @@ def create_main_workflow(data_dir, process_dir, soft, species, subjects,
                               # "outputnode.stereo_native_T1",
                               skull_t1_pipe, 'inputnode.stereo_native_T1')
 
-        main_workflow.connect(
-            segment_pnh_pipe, "outputnode.native_to_stereo_trans",
-            skull_t1_pipe, 'inputnode.native_to_stereo_trans')
-
         main_workflow.connect(datasource, "indiv_params",
                               skull_t1_pipe, 'inputnode.indiv_params')
 
