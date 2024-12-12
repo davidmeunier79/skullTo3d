39b769cc06033b780ee0c66b11f925939332e585
David Meunier <david_meunier_79@hotmail.fr>
2024-12-12 13:58:33 +0100

    Update ct pipe (#28)
    * align_ct_on_T1_2
    
    * flake8
    
    * added "align_ct_on_T1_2" by default
    
    * segment_skull

--- skullTo3d/pipelines/skull_pipe.py
+++ skullTo3d/pipelines/skull_pipe.py
@@ -409,13 +409,32 @@ def create_skull_ct_pipe(name="skull_ct_pipe", params={}):
     skull_ct_pipe.connect(inputnode, "native_T1",
                           align_ct_on_T1, "ref_file")
 
+    if "align_ct_on_T1_2" in params:
+
+        # align_ct_on_T1
+        align_ct_on_T1_2 = pe.Node(interface=RegAladin(),
+                                   name="align_ct_on_T1_2")
+
+        align_ct_on_T1_2.inputs.rig_only_flag = True
+
+        skull_ct_pipe.connect(align_ct_on_T1, 'res_file',
+                              align_ct_on_T1_2, "flo_file")
+
+        skull_ct_pipe.connect(inputnode, "native_T1",
+                              align_ct_on_T1_2, "ref_file")
+
     # align_ct_on_stereo_T1
     align_ct_on_stereo_T1 = pe.Node(
         interface=RegResample(pad_val=0.0),
         name="align_ct_on_stereo_T1")
 
-    skull_ct_pipe.connect(align_ct_on_T1, 'res_file',
-                          align_ct_on_stereo_T1, "flo_file")
+    if "align_ct_on_T1_2" in params:
+        skull_ct_pipe.connect(align_ct_on_T1_2, 'res_file',
+                              align_ct_on_stereo_T1, "flo_file")
+
+    else:
+        skull_ct_pipe.connect(align_ct_on_T1, 'res_file',
+                              align_ct_on_stereo_T1, "flo_file")
 
     skull_ct_pipe.connect(inputnode, 'native_to_stereo_trans',
                           align_ct_on_stereo_T1, "trans_file")
